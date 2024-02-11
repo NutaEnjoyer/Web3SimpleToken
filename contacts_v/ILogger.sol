2a46412c1d33b35e82882c34b512e192b2e812f7
Just Max <87654087+NutaEnjoyer@users.noreply.github.com>
2024-02-11 15:23:53 +0400

    Delete extra directory

--- contacts_v/ILogger.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.12 <0.9.0;
-
-interface ILogger{ 
-    function pay(address _from, uint _amount) external;
-    function getPayment(address _from, uint _number) external view returns(uint);
-}
